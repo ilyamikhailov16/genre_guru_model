49a66da815447babc69a7b01a3f2d07012b1976e
Ilya Mikhailov <ilya77716@yandex.ru>
2024-11-10 17:02:53 +0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,73 +1,123 @@
 import torch
-import pickle
-import numpy as np
+import torchaudio
 import pandas as pd
-import librosa
-import keras
-from tqdm import tqdm
+from torch.utils.data import Dataset
+from sklearn.preprocessing import LabelEncoder
 
-dataset = pd.read_csv("audios_dataset.csv", sep=";")
 
-train_data = dataset.sample(frac=0.75, random_state=0)
-valid_data = dataset.drop(train_data.index)
+class AudiosDataset(Dataset):
+    def __init__(
+        self,
+        annotation_file,
+        audio_dir,
+        num_samples,
+        target_sample_rate,
+        transform_func,
+        device,
+        label_encoder,
+    ):
+        self.annotation_file = pd.read_csv(annotation_file, sep=",")
+        self.audio_dir = audio_dir
+        self.num_samples = num_samples
+        self.target_sample_rate = target_sample_rate
+        self.device = device
+        self.transform_func = transform_func.to(self.device)
+        self.label_encoder = label_encoder
 
-x_train = train_data.drop('genre_id', axis=1)
-y_train = train_data['genre_id']
-x_valid = valid_data.drop('genre_id', axis=1)
-y_valid = valid_data['genre_id']
+    def __len__(self):
+        return len(self.annotation_file)
 
-y_train = torch.FloatTensor(keras.utils.to_categorical(y_train, 19))
-y_valid = torch.FloatTensor(keras.utils.to_categorical(y_valid, 19))
-y_valid_for_accuracy = torch.FloatTensor(valid_data['genre_id'].to_list())
+    def __getitem__(self, index):
+        label = self._get_audio_sample_label(index)
+        signal, sr = torchaudio.load(self._get_audio_sample_path(index))
+        signal = signal.to(self.device)
+        signal = self._resample_if_necessary(signal, sr)
+        signal = self._mix_down_if_necessary(signal)
+        signal = self._cut_if_necessary(signal)
+        signal = self._right_pad_if_necessary(signal)
+        signal = self.transform_func(signal)
 
-pickle.dump(y_train, open('y_train.sav', 'wb'))
-pickle.dump(y_valid, open('y_valid.sav', 'wb'))
-pickle.dump(y_valid_for_accuracy, open('y_valid_for_accuracy.sav', 'wb'))
+        return signal, label
 
-# Функция для преобразования спектрограммы в изображение
-def spec_to_image(spec, eps=1e-6):
-  mean = spec.mean()
-  std = spec.std()
-  spec_norm = (spec - mean) / (std + eps)
-  spec_min, spec_max = spec_norm.min(), spec_norm.max()
-  spec_scaled = 255 * (spec_norm - spec_min) / (spec_max - spec_min)
-  spec_scaled = spec_scaled.astype(np.uint8)
-  return spec_scaled
+    def _get_audio_sample_path(self, index):
+        return f"{self.audio_dir}/{self.annotation_file.iloc[index, 0]}"
 
-# Функция для получения мел-спектрограммы из аудиофайла
-def get_melspectrogram_db(file_path, sr=44100, n_fft=2048, hop_length=512, n_mels=128, fmin=20, fmax=8300, top_db=80):
-  wav,sr = librosa.load(file_path, sr=sr)
-  if wav.shape[0]<5*sr:
-    wav=np.pad(wav,int(np.ceil((5*sr-wav.shape[0])/2)),mode='reflect')
-  else:
-    wav=wav[:5*sr]
-  spec=librosa.feature.melspectrogram(y=wav, sr=sr, n_fft=n_fft,
-              hop_length=hop_length,n_mels=n_mels,fmin=fmin,fmax=fmax)
-  spec_db=librosa.power_to_db(spec,top_db=top_db)
-  return spec_db
+    def _get_audio_sample_label(self, index):
+        return int(self.label_encoder.transform([self.annotation_file.iloc[index, 1]])[0])
 
-X_train = []
-X_valid = []
+    def _resample_if_necessary(self, signal, sr):
+        if sr != self.target_sample_rate:
+            resampler = torchaudio.transforms.Resample(sr, self.target_sample_rate)
+            signal = resampler(signal)
+        return signal
 
-for i in tqdm(x_train['audio'].index):
-  spec_tensor = spec_to_image(get_melspectrogram_db(f"audios/audio_{i}.wav"))[np.newaxis,...]
-  X_train.append(spec_tensor)
+    def _mix_down_if_necessary(self, signal):
+        if signal.shape[0] > 1:
+            signal = torch.mean(signal, dim=0, keepdim=True)
+        return signal
 
-for i in tqdm(x_valid['audio'].index):
-  spec_tensor = spec_to_image(get_melspectrogram_db(f"audios/audio_{i}.wav"))[np.newaxis,...]
-  X_valid.append(spec_tensor)
+    def _cut_if_necessary(self, signal):
+        if signal.shape[1] > self.num_samples:
+            signal = signal[:, : self.num_samples]
+        return signal
 
-pickle.dump(X_train, open('X_train.sav', 'wb'))
-pickle.dump(X_valid, open('X_valid.sav', 'wb'))
+    def _right_pad_if_necessary(self, signal):
+        length_signal = signal.shape[1]
+        if length_signal < self.num_samples:
+            num_missing_samples = self.num_samples - length_signal
+            last_dim_padding = (0, num_missing_samples)  # (left, right)
+            signal = torch.nn.functional.pad(signal, last_dim_padding)
+        return signal
 
-# X_train = pickle.load(open('X_train.sav', 'rb'))[:1001]
-# X_valid = pickle.load(open('X_valid.sav', 'rb'))[:1001]
-# X_train = np.array(X_train)
-# X_valid = np.array(X_valid)
-# X_train = torch.FloatTensor(X_train)
-# X_valid = torch.FloatTensor(X_valid)
 
-# y_train = pickle.load(open('y_train.sav', 'rb'))[:1001]
-# y_valid = pickle.load(open('y_valid.sav', 'rb'))[:1001]
-# y_valid_for_accuracy = pickle.load(open('y_valid_for_accuracy.sav', 'rb'))[:1001]
+NUM_SAMPLES = 661500
+SAMPLE_RATE = 22050
 
+get_melspectrogram = torchaudio.transforms.MelSpectrogram(
+    sample_rate=SAMPLE_RATE, n_fft=1024, hop_length=512, n_mels=64
+)
+
+label_encoder = LabelEncoder()
+label_encoder.fit(
+    [
+        "blues",
+        "classical",
+        "country",
+        "disco",
+        "hiphop",
+        "jazz",
+        "metal",
+        "pop",
+        "reggae",
+        "rock",
+    ]
+)
+
+if torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
+print(f"Using device: ", device)
+
+train_dataset = AudiosDataset(
+    "GT-Music-Genre/train.csv",
+    "GT-Music-Genre",
+    NUM_SAMPLES,
+    SAMPLE_RATE,
+    get_melspectrogram,
+    device,
+    label_encoder,
+)
+
+valid_dataset = AudiosDataset(
+    "GT-Music-Genre/valid.csv",
+    "GT-Music-Genre",
+    NUM_SAMPLES,
+    SAMPLE_RATE,
+    get_melspectrogram,
+    device,
+    label_encoder,
+)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
+valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=128, shuffle=False)
